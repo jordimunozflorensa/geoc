c51e560c741169e9e587ac6841389ccf084f4e59
jordimunozflorensa <jordi.munoz.florensa@estudiantat.upc.edu>
2024-11-30 20:40:08 +0100

    pre final version of incremental delaunay implementation

--- lab5_files/delaunay_jordi.js
+++ lab5_files/delaunay_jordi.js
@@ -1,3 +1,48 @@
+// BOTTOM-UP CODE STRUCTURE (to ease readability)
+
+// ----------------------------------- General Functions -----------------------------------
+function mod(a, n) {
+    return ((a % n) + n) % n;
+}
+
+function det(p, q, r) {
+    return (q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y);
+}
+
+function submatrix(M, row, column) {
+    const ret = new Array(M.length);
+    for (let i = 0; i < ret.length; i++) {
+        ret[i] = [...M[i]];
+    }
+
+    ret.splice(row, 1);
+    for (let i = 0; i < ret.length; i++) {
+        ret[i].splice(column, 1);
+    }
+
+    return ret;
+}
+
+
+function matrixDeterminant(M) {
+    if (M.length == 2) {
+        return M[0][0] * M[1][1] - M[0][1] * M[1][0];
+    } else {
+        return M[0][0] * matrixDeterminant(submatrix(M, 0, 0)) - M[0][1] * matrixDeterminant(submatrix(M, 0, 1)) + M[0][2] * matrixDeterminant(submatrix(M, 0, 2));
+    }
+}
+
+function detProjectedPoints(p, a, b, c, dcel) {
+    const M = [
+        [b.x - a.x, b.y - a.y, (b.x - a.x) * (b.x + a.x) + (b.y - a.y) * (b.y + a.y)],
+        [c.x - a.x, c.y - a.y, (c.x - a.x) * (c.x + a.x) + (c.y - a.y) * (c.y + a.y)],
+        [p.x - a.x, p.y - a.y, (p.x - a.x) * (p.x + a.x) + (p.y - a.y) * (p.y + a.y)]
+    ];
+    return matrixDeterminant(M);
+}
+
+// ----------------------------------- DCEL Structure -----------------------------------
+
 class DCEL {
     constructor() {
         this.tableOfVertices = [];
@@ -62,6 +107,8 @@ class DCEL {
         this.numberOfFaces += 2;
 
         this.computeNewFaces(faceEdges, newVertex, newFaces, newEdges, newTwinEdges);
+        
+        // function to implement the loop for rotations
         transformToDelaunayDCEL(newVertex, this);
     }
 
@@ -81,6 +128,8 @@ class DCEL {
         }
     }
 }
+
+// ----------------------------------- Delaunay Triangulation -----------------------------------
 
 // recursive function that replaces the tree structure to finde the face
 function findFaceFromEdge(auxEdge, point, dcel) {
@@ -125,196 +174,11 @@ function getFaceVertices(faceIndex, dcel) {
     return vertices;
 }
 
-function transformToDelaunayDCEL(newVertex, dcel) {
-    const startingEdge = dcel.tableOfVertices[newVertex].edge;
-    let currentEdge = startingEdge;
-    let hasBeenRotation = false;
-
-    do {
-        hasBeenRotation = false;
-        const oppositeEdge = dcel.tableOfEdges[currentEdge].nextEdge;
-        const adjacentFace = dcel.tableOfEdges[dcel.tableOfEdges[oppositeEdge].twinEdge].rightFace;
-
-        // if no infinite face and triangle contains newVertex, then rotate edge
-        if (adjacentFace != -1 && isInsideTriangleCircle(newVertex, adjacentFace, dcel)) {
-            rotateEdge(oppositeEdge, dcel);
-            hasBeenRotation = true;
-        } else {
-            const lastEdge = dcel.tableOfEdges[oppositeEdge].nextEdge;
-            currentEdge = dcel.tableOfEdges[lastEdge].twinEdge;
-        }
-    } while (currentEdge != startingEdge || hasBeenRotation);
-}
-
 function isInsideTriangleCircle(vertex, face, dcel) {
     const faceVertices = getFaceVertices(face, dcel);
-    return isPointInCircle(dcel.tableOfVertices[vertex], [dcel.tableOfVertices[faceVertices[0]], dcel.tableOfVertices[faceVertices[1]], dcel.tableOfVertices[faceVertices[2]]], dcel);
-}
-
-function rotateEdge(edge, dcel) {
-    // identify data requiring update
-    const twinEdge = dcel.tableOfEdges[edge].twinEdge;
-
-    const e0 = dcel.tableOfEdges[edge].nextEdge;
-    const e1 = dcel.tableOfEdges[e0].nextEdge;
-    const e2 = dcel.tableOfEdges[twinEdge].nextEdge;
-    const e3 = dcel.tableOfEdges[e2].nextEdge;
-
-    const f0 = dcel.tableOfEdges[e0].rightFace;
-    const f1 = dcel.tableOfEdges[e2].rightFace;
-
-    const v0 = dcel.tableOfEdges[e0].sourceVertex;
-    const v1 = dcel.tableOfEdges[e2].sourceVertex;
-
-    // rotate edge counterclockwise
-    const edgeData = dcel.tableOfEdges[edge];
-    const twinEdgeData = dcel.tableOfEdges[twinEdge];
-
-    edgeData.sourceVertex = dcel.tableOfEdges[e1].sourceVertex;
-    edgeData.nextEdge = e3;
-
-    twinEdgeData.sourceVertex = dcel.tableOfEdges[e3].sourceVertex;
-    twinEdgeData.nextEdge = e1;
-
-    // update affected edges
-    dcel.tableOfEdges[e0].nextEdge = edge;
-
-    dcel.tableOfEdges[e1].nextEdge = e2;
-    dcel.tableOfEdges[e1].rightFace = f1;
-
-    dcel.tableOfEdges[e2].nextEdge = twinEdge;
-
-    dcel.tableOfEdges[e3].nextEdge = e0;
-    dcel.tableOfEdges[e3].rightFace = f0;
-
-    // update affected faces
-    dcel.tableOfFaces[f0].edge = edge;
-    dcel.tableOfFaces[f1].edge = twinEdge;
-
-    // update affected vertices
-    dcel.tableOfVertices[v0].edge = e0;
-    dcel.tableOfVertices[v1].edge = e2;
-}
-
-function getVertexEdges(dcel, vertex) {
-    const edges = [];
-
-    const startingEdge = dcel.tableOfVertices[vertex].edge;
-    let currentEdge = startingEdge;
-    do {
-        edges.push(currentEdge);
-        const triangleEdge2 = dcel.tableOfEdges[currentEdge].nextEdge;
-        const triangleEdge3 = dcel.tableOfEdges[triangleEdge2].nextEdge;
-
-        currentEdge = dcel.tableOfEdges[triangleEdge3].twinEdge;
-    } while (currentEdge != startingEdge);
-
-    return edges;
-}
-
-function removeEdge(dcel, edge) {
-    const edgeTwin = dcel.tableOfEdges[edge].twinEdge;
-    const faceRight = dcel.tableOfEdges[edge].rightFace;
-    const faceLeft = dcel.tableOfEdges[edgeTwin].rightFace;
-
-    dcel.tableOfFaces[faceRight] = null;
-    dcel.tableOfFaces[faceLeft] = null;
-
-    // TODO: update other affected DCEL data 
-    // if it needs to be used in the future after deletions
-}
-
-function pruneBoundaries(dcel, boundaries) {
-    boundaries.forEach(boundary => {
-        if (boundary.length == 1) {
-            const vertex = boundary[0] + 3; // we need to consider the enclosing triangle points added first
-            const edges = getVertexEdges(dcel, vertex);
-            edges.forEach(edge => {
-                removeEdge(dcel, edge);
-            });
-        } else {
-            for (let i = 0; i < boundary.length; i++) {
-                const vertex = boundary[i] + 3;
-                const previousVertex = boundary[mod(i - 1, boundary.length)] + 3;
-                const nextVertex = boundary[mod(i + 1, boundary.length)] + 3;
-
-                const edges = getVertexEdges(dcel, vertex);
-                edges.forEach(edge => {
-                    const dstVertex = dcel.tableOfEdges[dcel.tableOfEdges[edge].twinEdge].sourceVertex;
-
-                    // we need to check if local boundary is convex or concave
-                    const turn = det(dcel.tableOfVertices[previousVertex], dcel.tableOfVertices[vertex], dcel.tableOfVertices[nextVertex]);
-
-                    const det1 = det(dcel.tableOfVertices[previousVertex], dcel.tableOfVertices[vertex], dcel.tableOfVertices[dstVertex]);
-                    const det2 = det(dcel.tableOfVertices[vertex], dcel.tableOfVertices[nextVertex], dcel.tableOfVertices[dstVertex]);
-
-                    if ((turn < 0 && det1 < 0 && det2 < 0) || (turn >= 0 && (det1 < 0 || det2 < 0))) {
-                        removeEdge(dcel, edge);
-                    }
-                });
-            }
-        }
-    });
-}
-
-function addEnclosingTriangle(dcel, points) {
-    if (dcel.enclosingTriangleAdded) {
-        points.pop();
-        points.pop();
-        points.pop();
-    }
-
-    const box = getBoundingBox(points);
-
-    const xOffset = (box.xmax - box.xmin) * 0.1;
-    const yOffset = (box.ymax - box.ymin) * 0.1;
-
-    const p0 = {
-        x: (box.xmin + box.xmax) / 2,
-        y: box.ymax + (box.ymax - box.ymin)
-    };
-    const p1 = {
-        x: box.xmax + (box.xmax - box.xmin) / 2 + xOffset,
-        y: box.ymin - yOffset
-    };
-    const p2 = {
-        x: box.xmin - (box.xmax - box.xmin) / 2 - xOffset,
-        y: box.ymin - yOffset
-    };
-
-    points.push(p0, p1, p2);
-    dcel.enclosingTriangleAdded = true;
-}
-
-function getBoundingBox(points) {
-    let xmin, xmax, ymin, ymax;
-    xmin = xmax = points[0].x;
-    ymin = ymax = points[0].y;
-
-    points.forEach(point => {
-        if (point.x < xmin) xmin = point.x;
-        else if (point.x > xmax) xmax = point.x;
-
-        if (point.y < ymin) ymin = point.y;
-        else if (point.y > ymax) ymax = point.y;
-    });
-
-    return { xmin: xmin, xmax: xmax, ymin: ymin, ymax: ymax };
-}
-
-function isPointInTriangle(p, triangle, dcel) {
-    // determinant signs of the triangles segments
-    const d_s1 = Math.sign(det(triangle[0], triangle[1], p));
-    const d_s2 = Math.sign(det(triangle[1], triangle[2], p));
-    const d_s3 = Math.sign(det(triangle[2], triangle[0], p));
-
-    // we check where the point lies for every segment (-1:'sideA, 0:'onLine', 1:sideB) to study its case
-    const insideTriangle = (d_s1 == d_s2 && d_s1 == d_s3);
-    const onEdge = (d_s1 == 0 && d_s2 == d_s3) || (d_s2 == 0 && d_s1 == d_s3) || (d_s3 == 0 && d_s1 == d_s2);
-    const onCorner = Math.abs(d_s1) + Math.abs(d_s2) + Math.abs(d_s3) == 1;
-
-    // TODO: change output when necessary
-    return (insideTriangle || onEdge || onCorner);
+    return isPointInCircle(dcel.tableOfVertices[vertex], [dcel.tableOfVertices[faceVertices[0]], 
+                                                        dcel.tableOfVertices[faceVertices[1]], 
+                                                        dcel.tableOfVertices[faceVertices[2]]], dcel);
 }
 
 function isPointInCircle(p, circle_points, dcel) {
@@ -323,53 +187,200 @@ function isPointInCircle(p, circle_points, dcel) {
     const c = circle_points[2];
 
     const clockOrder = Math.sign(det(a, b, c));
-    const planeSide = Math.sign(det_projected_points(p, a, b, c, dcel));
+    const planeSide = Math.sign(detProjectedPoints(p, a, b, c, dcel));
 
+    // we have to check the order too
     return planeSide == 0 || planeSide != clockOrder;
 }
 
-function mod(a, n) {
-    return ((a % n) + n) % n;
-}
+function transformToDelaunayDCEL(newVertex, dcel) {
+    const startingEdge = dcel.tableOfVertices[newVertex].edge;
+    let currentEdge = startingEdge;
+    let hasBeenRotation = false;
 
-function submatrix(M, row, column) {
-    const ret = new Array(M.length);
-    for (let i = 0; i < ret.length; i++) {
-        ret[i] = [...M[i]];
-    }
+    for (let i = 0; i < dcel.numberOfEdges; i++) {
+        hasBeenRotation = false;
+        const oppositeEdge = dcel.tableOfEdges[currentEdge].nextEdge;
+        const adjacentFace = dcel.tableOfEdges[dcel.tableOfEdges[oppositeEdge].twinEdge].rightFace;
 
-    ret.splice(row, 1);
-    for (let i = 0; i < ret.length; i++) {
-        ret[i].splice(column, 1);
-    }
+        // case when we have to rotate the edge (point inside the circumcircle)
+        // check also that the face is not the infinite one
+        if (adjacentFace != -1 && isInsideTriangleCircle(newVertex, adjacentFace, dcel)) {
+            edgeRotation(oppositeEdge, dcel);
+            hasBeenRotation = true;
+        } else {
+            const lastEdge = dcel.tableOfEdges[oppositeEdge].nextEdge;
+            currentEdge = dcel.tableOfEdges[lastEdge].twinEdge;
+        }
 
-    return ret;
-}
-
-function det(p, q, r) {
-    return (q.x - p.x) * (r.y - p.y) - (r.x - p.x) * (q.y - p.y);
-}
-
-function matrixDeterminant(M) {
-    if (M.length == 2) {
-        return M[0][0] * M[1][1] - M[0][1] * M[1][0];
-    } else {
-        return M[0][0] * matrixDeterminant(submatrix(M, 0, 0)) - M[0][1] * matrixDeterminant(submatrix(M, 0, 1)) + M[0][2] * matrixDeterminant(submatrix(M, 0, 2));
+        if (currentEdge == startingEdge && !hasBeenRotation) {
+            break;
+        }
     }
 }
 
-function det_projected_points(p, a, b, c, dcel) {
-    const M = [
-        [b.x - a.x, b.y - a.y, (b.x - a.x) * (b.x + a.x) + (b.y - a.y) * (b.y + a.y)],
-        [c.x - a.x, c.y - a.y, (c.x - a.x) * (c.x + a.x) + (c.y - a.y) * (c.y + a.y)],
-        [p.x - a.x, p.y - a.y, (p.x - a.x) * (p.x + a.x) + (p.y - a.y) * (p.y + a.y)]
-    ];
-    return matrixDeterminant(M);
+function edgeRotation(edge, dcel) {
+    const twinEdge = dcel.tableOfEdges[edge].twinEdge;
+
+    const edge0 = dcel.tableOfEdges[edge].nextEdge;
+    const edge1 = dcel.tableOfEdges[edge0].nextEdge;
+    const edge2 = dcel.tableOfEdges[twinEdge].nextEdge;
+    const edge3 = dcel.tableOfEdges[edge2].nextEdge;
+
+    const vertex0 = dcel.tableOfEdges[edge0].sourceVertex;
+    const vertex1 = dcel.tableOfEdges[edge2].sourceVertex;
+
+    const face0 = dcel.tableOfEdges[edge0].rightFace;
+    const face1 = dcel.tableOfEdges[edge2].rightFace;
+
+    // edge rotation in counter-clockwise direction
+    const edgeAtributes = dcel.tableOfEdges[edge];
+    const twinEdgeAtributes = dcel.tableOfEdges[twinEdge];
+
+    edgeAtributes.sourceVertex = dcel.tableOfEdges[edge1].sourceVertex;
+    edgeAtributes.nextEdge = edge3;
+
+    twinEdgeAtributes.sourceVertex = dcel.tableOfEdges[edge3].sourceVertex;
+    twinEdgeAtributes.nextEdge = edge1;
+
+    // update the structures that are affected by the rotation
+    dcel.tableOfEdges[edge0].nextEdge = edge;
+
+    dcel.tableOfEdges[edge1].nextEdge = edge2;
+    dcel.tableOfEdges[edge1].rightFace = face1;
+
+    dcel.tableOfEdges[edge2].nextEdge = twinEdge;
+
+    dcel.tableOfEdges[edge3].nextEdge = edge0;
+    dcel.tableOfEdges[edge3].rightFace = face0;
+
+    dcel.tableOfVertices[vertex0].edge = edge0;
+    dcel.tableOfVertices[vertex1].edge = edge2;
+
+    dcel.tableOfFaces[face0].edge = edge;
+    dcel.tableOfFaces[face1].edge = twinEdge;
 }
+
+// ----------------------------------- Pruning Boundaries -----------------------------------
+
+function removeEdge(dcel, edge) {
+    const edgeTwin = dcel.tableOfEdges[edge].twinEdge;
+    const faceRight = dcel.tableOfEdges[edge].rightFace;
+
+    // we can use the twin face since we are in the right face
+    const faceLeft = dcel.tableOfEdges[edgeTwin].rightFace;
+
+    dcel.tableOfFaces[faceRight] = null;
+    dcel.tableOfFaces[faceLeft] = null;
+}
+
+function getVertexEdges(dcel, vertex) {
+    const edges = [];
+    const startingEdge = dcel.tableOfVertices[vertex].edge;
+    let currentEdge = startingEdge;
+
+    for (let i = 0; i < dcel.numberOfEdges; i++) {
+        edges.push(currentEdge);
+        const triangleEdge2 = dcel.tableOfEdges[currentEdge].nextEdge;
+        const triangleEdge3 = dcel.tableOfEdges[triangleEdge2].nextEdge;
+
+        currentEdge = dcel.tableOfEdges[triangleEdge3].twinEdge;
+        if (currentEdge == startingEdge) break;
+    }
+
+    return edges;
+}
+
+function pruneBoundaries(dcel, boundaries) {
+    for (let b = 0; b < boundaries.length; b++) {
+        const boundary = boundaries[b];
+
+        if (boundary.length == 1) {
+            // we have to remove all the edges that are connected to the vertex
+            const vertex = boundary[0] + 3;
+            const edges = getVertexEdges(dcel, vertex);
+            for (let e = 0; e < edges.length; e++) {
+                removeEdge(dcel, edges[e]);
+            }
+        } 
+        // we have to remove the edges that are not convex
+        else {
+            for (let i = 0; i < boundary.length; i++) {
+                const vertex = boundary[i] + 3;
+                const previousVertex = boundary[mod(i - 1, boundary.length)] + 3;
+                const nextVertex = boundary[mod(i + 1, boundary.length)] + 3;
+
+                const edges = getVertexEdges(dcel, vertex);
+                for (let e = 0; e < edges.length; e++) {
+                    const edge = edges[e];
+                    const dstVertex = dcel.tableOfEdges[dcel.tableOfEdges[edge].twinEdge].sourceVertex;
+
+                    // we need to check if local boundary is convex or concave compared to the global boundary
+                    const turn = det(dcel.tableOfVertices[previousVertex], dcel.tableOfVertices[vertex], dcel.tableOfVertices[nextVertex]);
+
+                    const det1 = det(dcel.tableOfVertices[previousVertex], dcel.tableOfVertices[vertex], dcel.tableOfVertices[dstVertex]);
+                    const det2 = det(dcel.tableOfVertices[vertex], dcel.tableOfVertices[nextVertex], dcel.tableOfVertices[dstVertex]);
+                    
+                    // if the edge is not convex compared to the boundary, we remove it
+                    if ((turn < 0 && det1 < 0 && det2 < 0) || (turn >= 0 && (det1 < 0 || det2 < 0))) {
+                        removeEdge(dcel, edge);
+                    }
+                }
+            }
+        }
+    }
+}
+
+// ----------------------------------- Containing Triangle -----------------------------------
+
+function getBoundingBox(points) {
+    var xmin, xmax, ymin, ymax;
+    xmin = xmax = points[0].x;
+    ymin = ymax = points[0].y;
+
+    for (var i = 1; i < points.length; i++) {
+        var point = points[i];
+        if (point.x < xmin) xmin = point.x;
+        else if (point.x > xmax) xmax = point.x;
+
+        if (point.y < ymin) ymin = point.y;
+        else if (point.y > ymax) ymax = point.y;
+    }
+
+    return {xmin: xmin, xmax: xmax, ymin: ymin, ymax: ymax};
+}
+
+function computeContainingTriangle(dcel, points) {
+    const boundingBox = getBoundingBox(points);
+
+    const offsetSmallCases = 0.2;
+    const offset = offsetSmallCases;
+
+    const offsetX = (boundingBox.xmax - boundingBox.xmin) * offset;
+    const offsetY = (boundingBox.ymax - boundingBox.ymin) * offset;
+
+    const p0 = {
+        x: boundingBox.xmin - (boundingBox.xmax - boundingBox.xmin) / 2 - offsetX,
+        y: boundingBox.ymin - offsetY
+    };
+    const p1 = {
+        x: (boundingBox.xmin + boundingBox.xmax) / 2,
+        y: boundingBox.ymax + (boundingBox.ymax - boundingBox.ymin)
+    };
+    const p2 = {
+        x: boundingBox.xmax + (boundingBox.xmax - boundingBox.xmin) / 2 + offsetX,
+        y: boundingBox.ymin - offsetY
+    };
+
+    points.push(p0, p1, p2);
+    dcel.enclosingTriangleAdded = true;
+}
+
+// -------------------------------------- Triangulation --------------------------------------
 
 function computeTriangulation(points, boundaries) {
     const dcel = new DCEL();
-    addEnclosingTriangle(dcel, points);
+    computeContainingTriangle(dcel, points);
     const N = points.length;
 
     dcel.initDCEL(points[N - 3], points[N - 2], points[N - 1]);
@@ -386,8 +397,6 @@ function computeTriangulation(points, boundaries) {
         if (dcel.tableOfFaces[i] != null) {
             const faceVertices = getFaceVertices(i, dcel);
             outputTriangles.push([mod(faceVertices[2] - 3, N), mod(faceVertices[1] - 3, N), mod(faceVertices[0] - 3, N)]); // Store INDICES, not points
-        } else {
-            console.log("Face ignored");
         }
     }
     return outputTriangles;
